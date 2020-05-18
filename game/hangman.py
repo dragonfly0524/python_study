a27a7a382dc57084ab311bb739cb71f33f9f051a
Umekawa Kei <33537662+dragonfly0524@users.noreply.github.com>
2020-05-19 00:33:03 +0900

    Add files via upload

--- game/hangman.py
+++ game/hangman.py
@@ -1,20 +1,40 @@
 #ハングマン定義
 def hangman(word):
-	wrong = 0
-	stages = ["",
-             "________        ",
-             "|               ",
-             "|        |      ",
-             "|        0      ",
-             "|       /|\     ",
-             "|       / \     ",
-             "|               "
-              ]
-    rletters = list(word)
-    board = ["_"] * len(word)
-    win = False
-    print("ハングマンへようこそ")
-
-#ループ処理
-while wrong < len(stages) - 1:
-	
+        wrong = 0
+        stages = ["",
+                "________        ",
+                "|               ",
+                "|        |      ",
+                "|        0      ",
+                "|       /|\     ",
+                "|       / \     ",
+                "|               "
+                ]
+        rletters = list(word)
+        board = ["_"] * len(word)
+        win = False
+        print("hangmanへようこそ")
+        while wrong < len(stages) - 1:
+                print("\n")
+                msg = "1文字を予想してね"
+                char = input(msg)
+                if char in rletters:
+                        cind = rletters.index(char)
+                        board[cind] = char
+                        rletters[cind] ='$'
+                else:
+                        wrong += 1
+                print(" ".join(board))
+                e = wrong+1
+                print("\n".join(stages[0:e]))
+#勝ち
+                if "_" not in board:
+                        print("あなたの勝ち!")
+                        print(" ".join(board))
+                        win = True
+                        break
+#負け
+        if not win:
+                print("\n".join(stages[0:wrong+1]))
+                print("あなたの負け!正解は{}.".format(word))
+hangman("cat")
