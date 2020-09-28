07ee26ca4b717cd903341fcf0d13f154816817c2
Umekawa Kei <33537662+dragonfly0524@users.noreply.github.com>
2020-09-28 16:46:57 +0900

    Add files via upload

--- marketing_python/task1_file-ope.py
+++ marketing_python/task1_file-ope.py
@@ -22,3 +22,28 @@ for filename in files:
     ext = filename.split('.')[-1]
     to_path = path + '/' + ext
     shutil.move(from_path, to_path)
+    
+#効率化すると以下
+import os
+import shutil
+
+path = './task1/data'
+files = os.listdir(path)
+
+for filename in files:
+    from_path = path + '/' + filename
+    if os.path.isfile(filename):
+        ext = filename.split('.')[-1]
+        to_path = path + '/' + ext
+        if not os.path.exists(to_path):
+            os.mkdir(to_path)
+        shutil.move(from_path, to_path)
+
+
+
+for filename in files:
+
+    ext = filename.split('.')[-1]
+
+
+    
---
+++ marketing_python/task2_excel-merge.py
@@ -0,0 +1,52 @@
+import glob
+import openpyxl
+
+filelist = glob.glob('./task2/data/*.xlsx')
+
+#書き込み用のファイル作成
+
+out_workbook = openpyxl.Workbook()
+out_sheet = out_workbook.active
+out_sheet.title = '月報一覧'
+
+out_sheet['A1'].value = '報告日'
+out_sheet['B1'].value = '支店'
+out_sheet['C1'].value = '担当者'
+out_sheet['D1'].value = '売上目標(千円)'
+out_sheet['E1'].value = '売上実績(千円)'
+out_sheet['F1'].value = '差異(千円)'
+out_sheet['G1'].value = '達成率(%)'
+
+#ファイル一覧の取得
+file = filelist[0]
+filelist = glob.glob('./task2/data/*.xlsx')
+
+#読み取り書き込みの繰り返し処理
+for i, file in enumerate(filelist):
+    workbook = openpyxl.load_workbook(file, data_only=True)
+    sheet = workbook['業務報告書']
+
+    #ファイルからセルの値を抽出
+    report_date = sheet['X1'].value
+    office = sheet['X2'].value
+    person = sheet['X3'].value
+    sales_goal = sheet['H9'].value
+    sales_result = sheet['H10'].value
+    sales_diff = sheet['H11'].value
+    sales_percentage = sheet['H12'].value
+
+    #セルへ書き込み
+    out_sheet.cell(row=i+2,column=1).value = report_date
+    out_sheet.cell(row=i+2,column=2).value = office
+    out_sheet.cell(row=i+2,column=3).value = person
+    out_sheet.cell(row=i+2,column=4).value = sales_goal
+    out_sheet.cell(row=i+2,column=5).value = sales_result
+    out_sheet.cell(row=i+2,column=6).value = sales_diff
+    out_sheet.cell(row=i+2,column=7).value = sales_percentage
+
+    #表示形式の変更
+    out_sheet.cell(row=i+2,column=1).number_format = 'yyyy年mm月dd日'
+    out_sheet.cell(row=i+2,column=7).number_format = '0.0%'
+#ファイルの保存
+out_filename = './task2/output/月報一覧.xlsx'
+out_workbook.save(out_filename)
